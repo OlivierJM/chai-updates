7b8f9f8c2a7e0b8715e78910cfc9a24edd8a2938
Olivier JM <manolivier93@gmail.com>
2018-11-13 22:39:48 +0200

    added basic needs in the methods

---
+++ imports/api/posts/methods.js
@@ -0,0 +1,7 @@
+/**
+ * @name Posts
+ * @param { String } title
+ * @param { String } content
+ * @requires Dates
+ * @requires users ==> who has created this posts, the id can be gotten from the server
+ */
