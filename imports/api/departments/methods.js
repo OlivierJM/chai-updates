9d3924cce55bd5a9fcc7f1e2f1ae0bc8757ed38c
Olivier JM <manolivier93@gmail.com>
2018-11-17 22:44:48 +0200

    added methods for crud in departments

--- imports/api/departments/methods.js
+++ imports/api/departments/methods.js
@@ -1,4 +1,24 @@
+import { Meteor } from 'meteor/meteor'
+import { check } from 'meteor/check'
+import Departments from './departments'
+
 /**
  * @name Departments
  * @param { String } name
  */
+
+ // department
+ Meteor.methods({
+     createDepartment(name){
+        check(name, String)
+        Departments.insert({
+            name,
+        }, err => err ? console.log(err.reason) : console.log('good department'))
+     },
+     removeDepartment(_id){
+         check(_id, String)
+         Departments.remove({
+             _id,
+            }, err => err ? console.log(err.reason) : console.log('removed department'))
+     }
+ })
