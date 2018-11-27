9684b7043d7caec776f0e6f61b254cf0967072db
Olivier JM <manolivier93@gmail.com>
2018-11-27 12:57:06 +0200

    created a filescollection for leads colletion

--- imports/api/leaders/leaders.js
+++ imports/api/leaders/leaders.js
@@ -1,5 +1,77 @@
-import { Mongo } from 'meteor/mongo';
+import { MongoInternals } from 'meteor/mongo';
+import { FilesCollection } from 'meteor/ostrio:files';
+import Grid from 'gridfs-stream';
 
-export default Leaders = new Mongo.Collection('leaders');
+import fs from 'fs'; // eslint-disable-line
 
-// The above might be converted into a FilesCollection because of the upload of the photos
+let gfs;
+if (Meteor.isServer) {
+  gfs = Grid(MongoInternals.defaultRemoteCollectionDriver().mongo.db, MongoInternals.NpmModule);
+}
+
+const validTypes = ['png', 'jpg', 'jpeg'];
+
+export const leaders = new FilesCollection({
+  collectionName: 'leaders',
+  allowClientCode: true, // Disallow remove files from Client
+  onBeforeUpload(file) {
+    // Allow upload files under 2mb, and only in png/jpg/jpeg formats
+    if (file.size >= 2000000) {
+      return 'Please upload files, with size equal or less than 5GB';
+    } else if (!validTypes.includes(file.ext)) {
+      return `Please upload either one of the following formats ${validTypes.join()}`;
+    }
+    return true;
+  },
+  onAfterUpload(file) {
+    // Move file to GridFS
+    Object.keys(file.versions).forEach(versionName => {
+      const metadata = { versionName, fileId: file._id, storedAt: new Date() }; // Optional
+      const writeStream = gfs.createWriteStream({ filename: file.name, metadata });
+
+      fs.createReadStream(file.versions[versionName].path).pipe(writeStream);
+
+      writeStream.on(
+        'close',
+        Meteor.bindEnvironment(uploadedFile => {
+          const property = `versions.${versionName}.meta.gridFsFileId`;
+          // If we store the ObjectID itself, Meteor (EJSON?) seems to convert it to a
+          // LocalCollection.ObjectID, which GFS doesn't understand.
+          this.collection.update(file._id.toString(), {
+            $set: {
+              [property]: uploadedFile._id.toString(),
+            },
+          });
+          this.unlink(this.collection.findOne(file._id.toString()), versionName); // Unlink file by version from FS
+        }),
+      );
+    });
+  },
+  interceptDownload(http, file, versionName) {
+    const _id = (file.versions[versionName].meta || {}).gridFsFileId;
+    if (_id) {
+      const readStream = gfs.createReadStream({ _id });
+      readStream.on('error', err => {
+        throw err;
+      });
+      readStream.pipe(http.response);
+    }
+    return Boolean(_id);
+  },
+  onAfterRemove(images) {
+    images.forEach(image => {
+      Object.keys(image.versions).forEach(versionName => {
+        const _id = (image.versions[versionName].meta || {}).gridFsFileId;
+        if (_id) {
+          gfs.remove({ _id }, err => {
+            if (err) throw err;
+          });
+        }
+      });
+    });
+  },
+});
+
+if (Meteor.isServer) {
+  leaders.allowClient();
+}
