d49bf47b93eef2b7a72f9a8049dab295b79358da
Olivier JM <manolivier93@gmail.com>
2018-11-28 16:03:20 +0200

    added a select field on form

--- imports/ui/dashboard/CreatePost.jsx
+++ imports/ui/dashboard/CreatePost.jsx
@@ -1,39 +1,56 @@
-import React, { useState } from 'react'
-import { Meteor } from 'meteor/meteor'
+import React, { useState } from "react";
+import { Meteor } from "meteor/meteor";
+import { Select } from "antd";
 
-function createPost(){
+const Option = Select.Option;
 
-    const title = useFormValues('title')
-    const content = useFormValues('content')
-    const link = useFormValues('link')
+function createPost() {
+  const title = useFormValues("title");
+  const content = useFormValues("content");
+  const link = useFormValues("link");
+  const [type, setType ] = useState('image')
 
-    function handleCreatePost(e){
-        e.preventDefault()
-        Meteor.call('createPost', title.value, content.value, link.value)
-    }
-    return (
-        <form onSubmit={handleCreatePost}>
-            <input {...title}  /> 
-            <br />
-            <input {...content} />  
-            <br />
-            <input {...link}  />  
-            <button className='btn' role='submit'>
-               submit 
-            </button>
-        </form>
-    )
+  function handleCreatePost(e) {
+    e.preventDefault();
+    Meteor.call("createPost", title.value, content.value, link.value);
+  }
+  function handleChange(value){
+    setType(value)
+  }
+  return (
+    <form onSubmit={handleCreatePost}>
+      <input {...title} />
+      <br />
+      <input {...content} />
+      <br />
+      <input {...link} />
+      <br />
+      <div>
+        <Select
+          value={type}
+          style={{ width: 120 }}
+          onChange={handleChange}
+        >
+          <Option value="video">Video</Option>
+          <Option value="image">Image</Option>
+        </Select>
+      </div>
+      <button className="btn" role="submit">
+        submit
+      </button>
+    </form>
+  );
 }
 
-function useFormValues(initial){
-    const [value, setValue] = useState(initial)
-    function handleValueChange(e){
-        setValue(e.target.value)
-    }
-    return {
-        value,
-        onChange: handleValueChange
-    }
+function useFormValues(initial) {
+  const [value, setValue] = useState(initial);
+  function handleValueChange(e) {
+    setValue(e.target.value);
+  }
+  return {
+    value,
+    onChange: handleValueChange
+  };
 }
 
-export default createPost
+export default createPost;
