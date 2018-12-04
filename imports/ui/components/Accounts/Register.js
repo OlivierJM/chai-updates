4a0eb78f68605ef2e5bbdd0b3afd65a5cc18a9e7
Olivier JM <manolivier93@gmail.com>
2018-12-04 23:05:14 +0200

    added accounts files

---
+++ imports/ui/components/Accounts/Register.js
@@ -0,0 +1,144 @@
+import React, { useState } from "react";
+import { Link, Redirect, withRouter } from 'react-router-dom'
+import { Accounts } from 'meteor/accounts-base'
+import {useFormInput, useError, validatePassword} from './accountsUtils'
+
+function Register(props) {
+    const email = useFormInput('')
+    const name = useFormInput('')
+    const password = useFormInput('')
+    const confirmedPassword = useFormInput('')
+    const gender = useFormInput('')
+    const age = useFormInput('')
+    const isValid = validatePassword(password.value, confirmedPassword.value)
+    const {error, setError} = useError('')
+    const [isAuth, setAuth] = useState(false)
+    const { location: { pathname } } = props
+    
+    function handleRegister(e){
+      e.preventDefault()
+      if (!gender.value.length) {
+        setError('You need to choose a gender')
+        return;
+      }
+      if (!isValid) {
+        setError('There was a problem with the password')
+        return;
+      }
+      const profile = {
+        name: name.value,
+        age: age.value,
+        gender: gender.value,
+        createdAt: new Date(),
+        role: pathname === '/dashboard/register' ? 'admin' : 'user'
+        }
+        const user = {
+          email: email.value,
+          password: password.value,
+          profile,
+        }
+        Accounts.createUser(user, err => err ? setError(err.reason) : setAuth(true) )
+    }
+  if(isAuth){
+    return <Redirect to='/' />
+  }  
+  return (
+    <div className="row">
+      <div className="col s4" />
+      <div className="col s4 " style={{ paddingTop: 30, margin: 0 }}>
+        <div className="card">
+          <div className="row">
+            <div className="col s12 center-align"> Register the {props.role}</div>
+            <form className="col s12" onSubmit={handleRegister}>
+              <div className="row">
+                <div className="input-field col s10" style={{ marginLeft: 15 }}>
+                  <input
+                    id="name"
+                    type="text"
+                    className="validate"
+                    {...name}
+                    required
+                  />
+                  <label htmlFor="name">Full Name</label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s10" style={{ marginLeft: 15 }}>
+                  <input
+                    id="email"
+                    type="email"
+                    className="validate"
+                    {...email}
+                    required
+                  />
+                  <label htmlFor="email">Email</label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s10" style={{ marginLeft: 15 }}>
+                  <input
+                    id="number"
+                    type="number"
+                    className="validate"
+                    {...age}
+                    required
+                  />
+                  <label htmlFor="number">Age</label>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="input-field col s10 " style={{ marginLeft: 15 }}>
+                  <input
+                    id="password"
+                    type="password"
+                    className="validate"
+                    required
+                    {...password}
+                  />
+                  <label htmlFor="password">Password</label>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="input-field col s10 " style={{ marginLeft: 15 }}>
+                  <input
+                    id="confirm-password"
+                    type="password"
+                    className="validate"
+                    name="confirm-password"
+                    required
+                    {...confirmedPassword}
+                  />
+                  <label htmlFor="confirm-password">Confirm Password</label>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="input-field center col s12">
+                  <button className="waves-effect waves-light btn">
+                    Register
+                  </button>
+                </div>
+              </div>
+              <div className='center row'>
+                <Link to='/login'>Login</Link>
+              </div>
+              <div className='center row'>
+                <p className='red-text'>
+                  {
+                    error.length ? error : null
+                  }
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+
+export default withRouter(Register)
---
+++ imports/ui/components/Accounts/accountsUtils.js
@@ -0,0 +1,62 @@
+// utilities for accounts
+import { useState } from "react";
+import { Meteor } from "meteor/meteor";
+
+/**
+ * @name useFormInput
+ * @param {String} initialValue 
+ * @description use state hooks to set the current value from the input field
+ * @returns { string, function }  containing the value and the onChange function
+ */
+export function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+  return {
+    value,
+    onChange: handleChange
+  };
+}
+/**
+ * @todo this can be enhanced
+ * @param {String} password 
+ * @param {String} password2 
+ * @description checks if the 2 given password are longer than 6chars and are equal
+ * @returns {Boolea}
+ */
+export function validatePassword(password, password2){
+    if (password.length < 6 ) {
+        return false
+    } else if (password.trim() !== password2.trim()) {
+        return false
+    } else {
+        return true
+    }
+}
+
+/**
+ * @param {String} initialError
+ * @returns {Object} error, setError
+ */
+export function useError(initialError){
+    const [error, setError] = useState(initialError)
+    return {
+        error,
+        setError
+    }
+}
+
+/**
+ * @description checks if the user is logged in or not
+ * @returns the role of the user is they are logged in and null if not logged in
+ */
+
+ function checkUserRole(){
+     const user =  Meteor.user()
+     if(!user){
+        return null
+    }
+    return user.profile.role
+ }
+ export { checkUserRole }
