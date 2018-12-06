83b3c345d6c852054048a190e618d6daad3edf35
Olivier JM <manolivier93@gmail.com>
2018-12-06 10:20:27 +0200

    added a logout route

--- imports/ui/components/Footer.jsx
+++ imports/ui/components/Footer.jsx
@@ -1,11 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState }  from "react";
+import { Link, Redirect } from "react-router-dom";
+import { Meteor } from 'meteor/meteor'; 
 
-const Footer = () => (
-  <footer className="border footer">
-    &copy; 2018 Communect
-    <button onClick={Meteor.logout()} className="button">Log Out</button>
-  </footer>
-);
+const Footer = () => {
+  const [isLoggedOut, setLogout] = useState(false)
+  const logOutUser = () => Meteor.logout(err => err ? console.log(err.reason) : setLogout(true))
+  // if this doesnt work, put it in the parent. 
+  if(isLoggedOut){
+     return <Redirect to='/login' />
+   }
+  return (
+      <footer className="border footer">
+        &copy; 2018 Communect
+        <button onClick={logOutUser} className="button">Log Out</button>
+      </footer>
+    )
+}
 
 export default Footer;
