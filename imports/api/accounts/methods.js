43a000bb2a0e50049cccc34adf4d2be5720513c6
Olivier JM <manolivier93@gmail.com>
2018-12-18 19:20:54 +0200

    added an adduser method

--- imports/api/accounts/methods.js
+++ imports/api/accounts/methods.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor'
+import { Accounts } from 'meteor/accounts-base'
 import { check } from 'meteor/check'
 import { PhoneNumbers } from './numbers' 
 
@@ -10,5 +11,9 @@ Meteor.methods({
             name, 
             number
         }, err => err && console.log(err.reason))
+    },
+    addUser(user){
+        check(user, Object)
+        Accounts.createUser(user)
     }
 })
