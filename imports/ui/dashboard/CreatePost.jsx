4155efa00b2db20b4b4e329186c722fe61935502
Olivier JM <manolivier93@gmail.com>
2018-12-28 11:17:58 +0200

    refactored create posts

--- imports/ui/dashboard/CreatePost.jsx
+++ imports/ui/dashboard/CreatePost.jsx
@@ -1,31 +1,42 @@
 import React, { useState } from "react";
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 import UploadImage from './leads/Upload'
 
 function createPost() {
-  const title = useFormValues("title")
-  const content = useFormValues("content")
-  const link = useFormValues("link")
+  const title = useFormValues("")
+  const content = useFormValues("")
+  const link = useFormValues("")
   const [type, setType ] = useState('image')
 
   function handleChange(event){
-    setType(event.target.value)
+      console.log(event.target.value)
+    setType(type)
   }
   return (
     <>
     <form>
-      <input {...title} />
+    <InputGroup>
+        <InputGroupAddon addonType="prepend">Title</InputGroupAddon>
+        <Input {...title} placeholder='The title goes here'/>
+      </InputGroup>
       <br />
-      <textarea {...content} />
+      <InputGroup>
+        <InputGroupAddon addonType="prepend">Text</InputGroupAddon>
+        <textarea {...content} rows={4} cols={50} placeholder='write your post content here'/>
+      </InputGroup>
       <br />
-      <input {...link} />
-
+      <InputGroup>
+        <InputGroupAddon addonType="prepend">Link</InputGroupAddon>
+        <Input {...link} placeholder="The Video or Image goes here" />
+      </InputGroup>
       <br />
-      <div className="form-group">
-        <select id="paperSelects1" defaultValue={type} onChange={handleChange}>
-          <option value="video">Video</option>
-          <option value="image">Image</option>
-        </select>
-      </div>
+      <InputGroup>
+        <InputGroupAddon addonType="prepend">Link Type</InputGroupAddon>
+            <Input type="select" defaultValue={type} onChange={handleChange}>
+              <option value="image">Image</option>
+              <option value="video">Video</option>
+            </Input>
+      </InputGroup>
     </form>
       <UploadImage title={title.value} type={type} content={content.value} link={link.value} />
     </>
