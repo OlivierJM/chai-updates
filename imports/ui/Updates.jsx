a668a5cc87605e67b814194c927eb0b8fb07498d
Olivier JM <manolivier93@gmail.com>
2019-01-16 13:50:16 +0200

    listing users and their details

--- imports/ui/Updates.jsx
+++ imports/ui/Updates.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
+import { Meteor } from 'meteor/meteor'
+import { withTracker } from "meteor/react-meteor-data";
+import {
+Table
+} from "reactstrap";
 
-function Updates(){
+function ListUsers({users}){
   return (
-    <div>
-      I like this one
-    </div>
+    <Table hover>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Email/Phone</th>
+        <th>Gender</th>
+      </tr>
+    </thead>
+    <tbody>
+      {users &&
+        users.map(user => (
+          <tr key={user._id}>
+            <td>{user.profile.name}</td>
+            <td>{user.username}</td>
+            <td>{user.profile.gender}</td>
+          </tr>
+        )) || null }
+    </tbody>
+  </Table>
   )
 }
 
-export default Updates
+export default withTracker(() => {
+  return {
+    users: Meteor.users.find({}).fetch()
+  }
+})(ListUsers)
 
 
