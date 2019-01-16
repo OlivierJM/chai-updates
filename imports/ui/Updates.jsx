0685a6be3a0b311edb4a2cda39ac11cf6563a5cf
Olivier JM <manolivier93@gmail.com>
2019-01-16 13:53:08 +0200

    added title on user list

--- imports/ui/Updates.jsx
+++ imports/ui/Updates.jsx
@@ -1,38 +1,39 @@
-import React from 'react'
-import { Meteor } from 'meteor/meteor'
-import { withTracker } from "meteor/react-meteor-data";
-import {
-Table
-} from "reactstrap";
+import React from "react"
+import { Meteor } from "meteor/meteor"
+import { withTracker } from "meteor/react-meteor-data"
+import { Table } from "reactstrap"
 
-function ListUsers({users}){
+function ListUsers({ users }) {
   return (
-    <Table hover>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Email/Phone</th>
-        <th>Gender</th>
-      </tr>
-    </thead>
-    <tbody>
-      {users &&
-        users.map(user => (
-          <tr key={user._id}>
-            <td>{user.profile.name}</td>
-            <td>{user.username}</td>
-            <td>{user.profile.gender}</td>
+    <>
+    <h3 className='text-center'>All Members</h3>
+      <Table hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email/Phone</th>
+            <th>Gender</th>
           </tr>
-        )) || null }
-    </tbody>
-  </Table>
+        </thead>
+        <tbody>
+          {(users &&
+            users.map(user => (
+              <tr key={user._id}>
+                <td>{user.profile.name}</td>
+                <td>{user.username}</td>
+                <td>{user.profile.gender}</td>
+              </tr>
+            ))) ||
+            null}
+        </tbody>
+      </Table>
+    </>
   )
 }
 
 export default withTracker(() => {
+  Meteor.subscribe("users")
   return {
     users: Meteor.users.find({}).fetch()
   }
 })(ListUsers)
-
-
